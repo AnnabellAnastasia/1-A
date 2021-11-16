a4ac6687aec2f85bf19208afc6d24b99afa3487a
Annabella Wilkins <oav2008@gmail.com>
2021-11-15 22:59:13 -0500

    updated class Budget and Main

--- src/Budget.java
+++ src/Budget.java
@@ -1,46 +1,141 @@
+///**
+// * Budjet.java
+// * @author annabellawilkins
+// * @since november 2021
+// * This class ...
+// */
+//public class Budget {
+///*instance variables for Budget attributes
+//* */
+//    private double hourlyWages;
+//    private double rentForLocalApartment;
+//    private double internetService;
+//    private double groceryBill;
+//    private double funAllowance;
+//
+//    /** constructor Budget initialize five instances variables to parameters
+//     *
+//     * @param initHourlyWages
+//     * @param rent
+//     * @param internet
+//     * @param initFunAllowance
+//     * @param initGroceryBill
+//     *
+//     */
+//    public Budget(double initHourlyWages,
+//                  double rent,
+//                  double internet,
+//                  double initFunAllowance,
+//                  double initGroceryBill){
+//
+//        hourlyWages = initHourlyWages;
+//        internetService = internet;
+//        rentForLocalApartment = rent;
+//        groceryBill = initGroceryBill;
+//        funAllowance = initFunAllowance;
+//    }
+//// The calculateMonthlyExpences() method calculate monthly
+//// and weekly expences, workhours and workhours including savings.
+//
+//    public void calculateMonthlyExpences(int rent,
+//                                           double internet,
+//                                           double grocery,
+//                                           double fun){
+//         int monthlyExpences = (int)(rent + internet + grocery + fun);
+//         int weeklyExpences = monthlyExpences / 4;
+//         int workHours = (int) (monthlyExpences / hourlyWages);
+//         int workSafeHours = (int)((monthlyExpences+100) / hourlyWages);
+//
+//         System.out.println("My hourly wage is " + hourlyWages);
+//        System.out.println("My montly expences are " + monthlyExpences);
+//        System.out.println("My total montly expences are " + monthlyExpences);
+//        System.out.println(workHours + " hours I will have to work to afford my monthly expenses");
+//        System.out.println(workSafeHours + " hours I  will have to work to include savings of $100 per month");
+//         if (workHours >= 160){
+//            System.out.println("Based on my monthly budget, I need a new job!!!!");
+//        }else
+//            System.out.println("Based on my monthly budget, I can keep current job!!!!");
+//
+//
+//    };
+//}
+
+/**
+ * Class Budget keeps truck of wage, rent, internet, grocery and fun expences.
+ */
 public class Budget {
-/*instance variables for Budget attributes
-* */
-    private double hourlyWages;
-    private double rentForLocalApartment;
-    private double internetService;
-    private double groceryBill;
-    private double funAllowance;
+    //instance variable names
+    private double wage;
+    private double rent;
+    private double internet;
+    private double grocery;
+    private double fun;
 
-// constructor: initialize all instances variables to parameters
-    public Budget(double initHourlyWages,
-                  double rent,
-                  double internet,
-                  double initFunAllowance,
-                  double initGroceryBill){
-
-        hourlyWages = initHourlyWages;
-        internetService = internet;
-        rentForLocalApartment = rent;
-        groceryBill = initGroceryBill;
-        funAllowance = initFunAllowance;
+    //Instance variable name
+    public Budget(double wage, double rent, double internet, double grocery, double fun) {
+        this.wage = wage;
+        this.rent = rent;
+        this.internet = internet;
+        this.grocery = grocery;
+        this.fun = fun;
     }
-/* method to print
-**/
-    public void calculateMonthlyExpences(int rent,
-                                           double internet,
-                                           double grocery,
-                                           double fun){
-         int monthlyExpences = (int)(rent + internet + grocery + fun);
-         int weeklyExpences = monthlyExpences / 4;
-         int workHours = (int) (monthlyExpences / hourlyWages);
-         int workSafeHours = (int)((monthlyExpences+100) / hourlyWages);
 
-         System.out.println("My hourly wage is " + hourlyWages);
-        System.out.println("My montly expences are " + monthlyExpences);
-        System.out.println("My total montly expences are " + monthlyExpences);
-        System.out.println(workHours + " hours I  will have to work to afford my monthly expenses");
-        System.out.println(workSafeHours + " hours I  will have to work to to include savings of $100 per month");
-         if (workHours >= 160){
-            System.out.println("Based on my monthly budget, I need a new job!!!!");
-        }else
-            System.out.println("Based on my monthly budget, I can keep current job!!!!");
+    // accessor methods - getters
+    public double getWage() {
+        return wage;
+    }
 
+    public void setWage(double wage) {
+        this.wage = wage;
+    }
 
-    };
+    public double getRent() {
+        return rent;
+    }
+
+    public void setRent(double rent) {
+        this.rent = rent;
+    }
+
+    public double getInternet() {
+        return internet;
+    }
+
+    public void setInternet(double internet) {
+        this.internet = internet;
+    }
+
+    public double getGrocery() {
+        return grocery;
+    }
+
+    public void setGrocery(double grocery) {
+        this.grocery = grocery;
+    }
+
+    public double getFun() {
+        return fun;
+    }
+
+    public void setFun(double fun) {
+        this.fun = fun;
+    }
+
+//    public void calcExpences(double wage, double rent, double internet, double grocery, double fun) {
+//        int monthlyExpences = (int) (rent + internet + grocery + fun);
+//        int weeklyExpences = monthlyExpences / 4;
+//        int workHours = (int) (monthlyExpences / hourlyWages);
+//        int workSafeHours = (int) ((monthlyExpences + 100) / hourlyWages);
+//
+//        System.out.println("My hourly wage is " + hourlyWages);
+//        System.out.println("My montly expences are " + monthlyExpences);
+//        System.out.println("My total montly expences are " + monthlyExpences);
+//        System.out.println(workHours + " hours I will have to work to afford my monthly expenses");
+//        System.out.println(workSafeHours + " hours I  will have to work to include savings of $100 per month");
+//        if (workHours >= 160) {
+//            System.out.println("Based on my monthly budget, I need a new job!!!!");
+//        } else
+//            System.out.println("Based on my monthly budget, I can keep current job!!!!");
+//    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
+/**
+ * Main.java
+ *
+ * @author annabellawilkins
+ * @since november 2021
+ * This class contains main method.
+ */
+//public class Main {
+//    // The main() method creates annabella's object
+//    public static void main(String[] args) {
+//        //call constructor Budget to create new budget
+//        Budget annabella = new Budget();
+//        //call annabella print method
+//        //annabella.print();
+//        annabella.calculateMonthlyExpences(1200,100.00,1000,500.25);
+//
+//    }
+//}
 public class Main {
+
     public static void main(String[] args) {
         //call constructor Budget to create new budget
-        Budget annabella = new Budget(15.75,1200.00,95.75,500.95,500.85);
-        //call annabella print method
-        //annabella.print();
-        annabella.calculateMonthlyExpences(1200,100.00,1000,500.25);
-
+        Budget tom = new Budget();
+        tom.setWage(40.00);
+        // call tom calcExpences method
+//        tom.calcExpences();
     }
 }
